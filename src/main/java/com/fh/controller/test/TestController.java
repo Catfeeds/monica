e9883a5f6b9ea5e32e0f0897531802f65b3fdd95
wufazhuce127 <dengjiacheng127@163.com>
2018-05-17 12:16:21 +0800

    monica

---
+++ src/main/java/com/fh/controller/test/TestController.java
@@ -0,0 +1,295 @@
+package com.fh.controller.test;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fh.controller.base.BaseController;
+import com.fh.dbutil.JdbcUtil;
+import com.fh.entity.Page;
+import com.fh.qy.qyutil.WeiXinParamesUtil;
+import com.fh.qy.qyutil.WeiXinUtil;
+import com.fh.qy.service.Contacts_DepartmentService;
+import com.fh.qy.service.Contacts_UserService;
+import com.fh.service.item.impl.ItemService;
+import com.fh.service.test.exp.impl.ExpService;
+import com.fh.service.wxqy.qywxuser.QyWxUserManager;
+import com.fh.service.wxqy.weixindepartment.impl.WeixinDepartmentService;
+import com.fh.util.Jurisdiction;
+import com.fh.util.PageData;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.hazelcast.nio.serialization.Data;
+
+
+@Controller
+@RequestMapping(value="/test1")
+public class TestController extends BaseController{
+	@Resource(name = "stockService")
+	ExpService expService;
+	@Resource(name="qywxuserService")
+	private QyWxUserManager qywxuserService;
+	@Resource(name="weixindepartmentService")
+	WeixinDepartmentService weixindepService;
+	@Resource(name="itemService")
+	ItemService itemService;
+	
+	@RequestMapping(value="/ceshi")
+	@ResponseBody
+	public  String getAjax(){
+		System.out.println("test_controller");
+		//stockService.lt();
+		return "成功";
+		
+	}
+
+
+	
+	@RequestMapping(value="/test_getUser")
+	@ResponseBody
+	public  Map<String, Object> test_getUser() throws Exception{
+		System.out.println("----------->:同步用户信息controller");
+		Map<String, Object> json = new HashMap<String, Object>();
+		//2.获取access_token:根据企业id和通讯录密钥获取access_token,并拼接请求url
+		String accessToken= WeiXinUtil.getAccessToken(WeiXinParamesUtil.corpId, WeiXinParamesUtil.contactsSecret).getToken();
+		//String accessToken2= WeiXinUtil.getAccessToken(WeiXinParamesUtil.corpId, WeiXinParamesUtil.contactsSecret).getToken();
+		System.out.println("accessToken:"+accessToken);
+		//System.out.println("accessToken:"+accessToken2);
+		//1.获取部门ID以及是否获取子部门成员
+		String departmentId="1";
+		String fetchChild="1";
+		Contacts_UserService cus = new Contacts_UserService();
+		JSONObject jsonObject = cus.getDepartmentUser(accessToken, departmentId, fetchChild);
+		JSONArray userlist=(JSONArray) jsonObject.get("userlist");
+		@SuppressWarnings("unchecked")
+		Iterator<Object> it = userlist.iterator();
+		String userid = null;
+		String openidString = null;
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		List<PageData> pages = qywxuserService.listAll(pd);
+		List<PageData> depPages = weixindepService.listAll(pd);
+		//System.out.println("成员集合："+pages);
+		//新增开关
+		int hint = 0; //开0,关1
+		//删除开关
+		int dint = 0; //开0,关1
+		int count = 1;
+		int dcount = 1;
+		int i;
+		String[] strs = null;
+		ArrayList<String>  depName = new ArrayList<String>();
+		while (it.hasNext()) {
+			JSONObject ob = (JSONObject) it.next();
+			strs = ob.getString("department").substring(1, ob.getString("department").length()-1).split(",");
+			for (i = 0; i < pages.size(); i++) {
+				if(ob.getString("userid").equals(pages.get(i).getString("USERID"))){
+					hint = 1;
+				}
+			}
+			if (hint != 1) {
+				userid = ob.getString("userid");
+				openidString = cus.useridToOpenid(accessToken, userid);
+				pd.put("QYWXUSER_ID",this.get32UUID());	//主键
+				pd.put("USERID",ob.getString("userid"));	
+				pd.put("NAME",ob.getString("name"));
+				pd.put("DEPNUM",ob.getString("department"));
+				for (int x = 0; x < strs.length; x++) {
+					System.out.println(strs[x].getClass());
+					for (int y = 0; y < depPages.size(); y++) {
+						if (strs[x].equals(depPages.get(y).get("ID").toString())) {
+							{
+								depName.add(depPages.get(y).getString("DNAME"));
+							}
+						}
+					}
+				}
+				//pd.put("DEPARTMENT",ob.getString("department"));	
+				pd.put("DEPARTMENT",depName.toString().substring(1, depName.toString().length()-1));
+				pd.put("OPENID",openidString);	
+				System.out.println("======>>"+pd);
+				qywxuserService.save(pd);
+				System.out.println("插入"+count+"条数据");
+				count ++ ;
+				depName.clear();
+			}
+			hint = 0;
+		}
+		/*for (int j = 0; j < pages.size(); j++) {
+			while (it.hasNext()) {
+				JSONObject obj = (JSONObject) it.next();
+				if(pages.get(j).getString("USERID").equals(obj.getString("userid"))){
+					dint = 1;
+				}
+			} 
+			if (dint != 1) {
+				pd.put("USERID",pages.get(j).getString("USERID"));
+				qywxuserService.delete(pd);
+			}
+			dint = 0;
+		}*/
+		json.put("result", "成功");
+		return json;
+		
+	}
+	//DengJiaCheng
+	
+	@RequestMapping(value="/test_getOpenId")
+	@ResponseBody
+	public  Map<String, Object> test_getOpenId() throws Exception{
+		System.out.println("----------->:getOpenid");
+		Map<String, Object> json = new HashMap<String, Object>();
+		//2.获取access_token:根据企业id和通讯录密钥获取access_token,并拼接请求url
+		String accessToken= WeiXinUtil.getAccessToken(WeiXinParamesUtil.corpId, WeiXinParamesUtil.contactsSecret).getToken();
+		System.out.println("accessToken:"+accessToken);
+		Contacts_UserService cus = new Contacts_UserService();
+		String openidString = cus.useridToOpenid(accessToken, "DengJiaCheng");
+		json.put("result", "成功");
+		return json;
+	}
+	
+	@RequestMapping(value="/test_getUserByDb")
+	@ResponseBody
+	public  Map<String, Object> test_getUserByDb() throws Exception{
+		Map<String, Object> json = new HashMap<String, Object>();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		String keywords = "2";				//关键词检索条件
+		if(null != keywords && !"".equals(keywords)){
+			pd.put("keywords", keywords.trim());
+		}
+		Page page = new Page();
+		page.setPd(pd);
+		List<PageData>	userList =qywxuserService.list(page);	
+		System.out.println(userList);
+		json.put("result", userList);
+		return json;
+	}
+	
+	@RequestMapping(value="/test_db")
+	@ResponseBody
+	public  Map<String, Object> test_db() throws Exception{
+		Map<String, Object> json = new HashMap<String, Object>();
+		/*T_ICItemExample t_ICItemExample = new T_ICItemExample();
+		T_ICItemExample.Criteria criteria = t_ICItemExample.createCriteria();
+		criteria.andFnameLike("浴巾");*/
+		/*List<T_ICItem> t_ICItems = t_icitemmapper.selectByOther(null);
+		System.out.println(t_ICItems.size());*/
+		int pn = 1;
+		PageHelper.startPage(pn,3);//每页的条数
+		JdbcUtil jdbcUtil = new JdbcUtil();
+		List<PageData> jsonObj = jdbcUtil.getAll("FW0231浴巾架＿有标");
+		PageInfo<PageData> varListPage=new PageInfo<PageData>(jsonObj,1); //5表示要连续显示的页数
+		System.out.println(jsonObj);
+		json.put("result",jsonObj);
+		return json;
+		
+		
+	}
+	
+
+	@RequestMapping(value="/test_dbItem")
+	@ResponseBody
+	public  Map<String, Object> test_dbItem(Page page) throws Exception{
+		Map<String, Object> json = new HashMap<String, Object>();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		String keywords = "浴巾";
+		pd.put("keywords", keywords.trim());
+		page.setPd(pd);
+		System.out.println(pd);
+		List<PageData>	varList = itemService.list(page);
+		System.out.println(varList);
+		System.out.println(varList.size());
+		json.put("result",varList);
+		return json;
+		
+		
+	}
+	
+
+	//z_ARSummary
+	/**
+	 * @Year		INT, --年份
+	 	@Period		INT, --期间
+		@CustNum	VARCHAR(255), --客户名称
+		@EmpName	VARCHAR(255) --业务员
+	 * 
+	 * <p>Title: test_z_ARSummary</p>
+	 * <p>Description: </p>
+	 * @param page
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/z_ARSummary")
+	@ResponseBody
+	public  Map<String, Object> test_z_ARSummary(Page page) throws Exception{
+		Map<String, Object> json = new HashMap<String, Object>();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		pd.put("Year", 2017);
+		pd.put("Period",2);
+		pd.put("CustNum","");
+		pd.put("EmpName","");
+		System.out.println(pd);
+		page.setPd(pd);
+		System.out.println(pd);
+		List<PageData>	varList = itemService.list_z_ARSummary(page);
+		System.out.println(varList);
+		System.out.println(varList.size());
+		json.put("result",varList);
+		return json;
+		
+		
+	}
+	
+	@RequestMapping(value="/load")
+	public ModelAndView load(Page page) throws Exception{
+		/*logBefore(logger, Jurisdiction.getUsername()+"列表NewForWx");*/
+		//if(!Jurisdiction.buttonJurisdiction(menuUrl, "cha")){return null;} //校验权限(无权查看时页面会有提示,如果不注释掉这句代码就无法进入列表页面,所以根据情况是否加入本句代码)
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		String keywords = pd.getString("keywords");				//关键词检索条件
+		String treeKey = pd.getString("treeKey");
+		String showCount = pd.getString("showCount");
+		if(null != keywords && !"".equals(keywords)){
+			pd.put("keywords", keywords.trim());
+		}
+		if(null != treeKey && !"".equals(treeKey)){
+			pd.put("treeKey", URLDecoder.decode(treeKey, "UTF-8").trim());
+		}
+		if(null == showCount || "".equals(showCount)){
+			page.setShowCount(150);
+		}
+		
+		page.setPd(pd);
+		List<PageData> list_tree =  itemService.list_tree(page);
+		List<PageData>	varList = itemService.list(page);
+		mv.addObject("varList", varList);
+		mv.addObject("list_tree", list_tree);
+		mv.setViewName("item/wxic");
+		mv.addObject("pd", pd);
+		//mv.addObject("QX",Jurisdiction.getHC());	//按钮权限
+		return mv;
+		
+		
+	}
+}
---
+++ src/main/java/com/fh/controller/test/TestSynchronizationController.java
@@ -0,0 +1,103 @@
+package com.fh.controller.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fh.controller.base.BaseController;
+import com.fh.entity.Page;
+import com.fh.service.management.supplier.impl.SupplierService;
+import com.fh.service.management.synchronization.impl.SynSupplierService;
+import com.fh.util.PageData;
+
+
+@Controller
+@RequestMapping(value="/test")
+public class TestSynchronizationController extends BaseController{
+	@Resource(name="synSupplierService")
+	private SynSupplierService synSupplierService;
+	@Resource(name="supplierService")
+	private SupplierService supplierService;
+
+	@RequestMapping(value="/test_get")
+	@ResponseBody
+	public  Map<String, Object> test_get() throws Exception{
+		Map<String, Object> json = new HashMap<String, Object>();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		Page page = new Page();
+		page.setPd(pd);
+		//新增开关
+		int hint = 0; //0为开启，1为关闭
+		//删除开关
+		int dint = 0; //开0,关1
+		//int dstr = 0;
+		int count = 0;
+		int dcount = 0;
+		List<PageData>	synList  =synSupplierService.supplier_synchronization(page);	//需要同步的数据
+		List<PageData>	varOList =supplierService.listAll(pd);  //本地数据
+		System.out.println("需要同步的数据"+synList.size());
+		System.out.println("本地数据"+varOList.size());
+		for (int i = 0; i < synList.size(); i++) {
+			hint = 1;
+			//hint = 1;
+			for (int j = 0; j < varOList.size(); j++) {
+				if(varOList.get(j).get("FITEMID").equals(synList.get(i).get("FItemID"))){
+					hint = 0;
+				}
+				/*if(!synList.get(i).get("FItemID").equals(varOList.get(j).get("FITEMID"))){
+					dint = 0; //不存在
+					dstr = (int) synList.get(i).get("FItemID");
+				}*/
+			}
+			if(hint == 1) {
+				 PageData pd1 = new PageData();
+				 pd1 = this.getPageData();
+				 pd1.put("SUPPLIER_ID", this.get32UUID());	//主键
+				 pd1.put("FITEMID", synList.get(i).get("FItemID"));
+				 pd1.put("FNUMBER", synList.get(i).get("FNumber"));
+				 pd1.put("FMODIFYTIME", synList.get(i).get("FModifyTime"));
+				 pd1.put("FNAME", synList.get(i).get("FName"));
+				 pd1.put("FADDRESS", synList.get(i).get("FAddress"));
+				 pd1.put("FCONTACT", synList.get(i).get("FContact"));
+				 pd1.put("FTELEPHONE",synList.get(i).get("FPhone"));
+				 supplierService.save(pd1);
+				 count ++ ;
+				// System.out.println(pd);
+			}
+			
+			
+		}
+		for (int j = 0; j < varOList.size(); j++) {
+			for (int i = 0; i < synList.size(); i++) {
+				if(synList.get(i).get("FItemID").equals(varOList.get(j).get("FITEMID"))){
+					dint = 1; //存在
+				}
+					//dstr = (int) synList.get(i).get("FItemID");
+			}
+			if(dint == 0){
+				System.out.println(varOList.get(j).get("FITEMID"));
+				PageData pd2 = new PageData();
+				pd2.put("FITEMID",(int)varOList.get(j).get("FITEMID"));
+				System.out.println("delete:"+pd2);
+				supplierService.deleteByFITEMID(pd2);
+				dcount ++ ;
+			}
+			dint = 0;
+		}
+		System.out.println("新增数据"+count);
+		System.out.println("删除数据"+dcount);
+		json.put("result", "123");
+		return json;
+	}
+	
+	
+
+
+}
