c1b924cf66a823d987c77d38f8ae15a5826dbe68
wufazhuce127 <dengjiacheng127@163.com>
2018-11-19 17:42:13 +0800

    deng

--- src/main/java/com/fh/util/Const.java
+++ src/main/java/com/fh/util/Const.java
@@ -36,7 +36,7 @@ public class Const {
 	public static final String FILEPATHFILE = "uploadFiles/file/";			//文件上传路径
 	public static final String FILEPATHFILEOA = "uploadFiles/uploadFile/";	//文件上传路径(oa管理)
 	public static final String FILEPATHTWODIMENSIONCODE = "uploadFiles/twoDimensionCode/"; //二维码存放路径
-	public static final String NO_INTERCEPTOR_PATH = ".*/((login)|(platformpage)|(commodity_shop)|(dataAnalysis)|(officeautomation)|(erp_Get)|(test_Get)|(http)|(logout)|(order)|(code)|(app)|(wx)|(qywx)|(store)|(platformQY)|(test)|(plugins)|(weixin)|(static)|(main)|(websocket)|(uploadImgs)).*";	//不对匹配该值的访问路径拦截（正则）
+	public static final String NO_INTERCEPTOR_PATH = ".*/((login)|(rematoget)|(platformpage)|(commodity_shop)|(dataAnalysis)|(officeautomation)|(erp_Get)|(test_Get)|(http)|(logout)|(order)|(code)|(app)|(wx)|(qywx)|(store)|(platformQY)|(test)|(plugins)|(weixin)|(static)|(main)|(websocket)|(uploadImgs)).*";	//不对匹配该值的访问路径拦截（正则）
 	public static ApplicationContext WEB_APP_CONTEXT = null; //该值会在web容器启动时由WebAppContextListener初始化
 	
 	/**
