c4ff19fda491aaf63029d1c05b0e1c2099314f72
wufazhuce127 <dengjiacheng127@163.com>
2018-11-20 11:15:03 +0800

    deng

--- src/main/java/com/fh/controller/test/RematoGetController.java
+++ src/main/java/com/fh/controller/test/RematoGetController.java
@@ -1,13 +1,16 @@
 package com.fh.controller.test;
 
 import com.fh.controller.base.BaseController;
+import com.fh.service.item.impl.ItemService;
 import com.fh.service.management.client.ClientManager;
 import com.fh.service.management.interfaceip.InterfaceIPManager;
 import com.fh.util.Const;
+import com.fh.util.DateUtil;
 import com.fh.util.PageData;
 import com.fh.util.Tools;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import org.junit.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +22,7 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,6 +36,9 @@ public class RematoGetController extends BaseController {
 
     @Resource(name="clientService")
     private ClientManager clientService;
+
+    @Resource(name="itemService")
+    ItemService itemService;
 
 
 
@@ -143,6 +150,7 @@ public class RematoGetController extends BaseController {
             System.out.println("修改数据"+ecount);
             System.out.println("删除数据"+dcount);
             json.put("Data", "新增数据"+count+"条；"+"修改数据"+ecount+"条；"+"删除数据"+dcount+"条。");
+            System.gc();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -181,4 +189,269 @@ public class RematoGetController extends BaseController {
         }
         return jsonarr;
     }
+
+    @RequestMapping(value="/test")
+    @ResponseBody
+    public Map<String, Object> test() throws Exception{
+        Map<String, Object> json = new HashMap<String, Object>();
+        String requestUrl = this.getIpAndProjectName()+"/erp_get/erp_cus";
+        //调用方式  0为远程接口方式，1为多数据源调用方式
+        String todoType = Tools.readTxtFile("admin/config/TYPE.txt");
+        JSONArray jsonarr = null;
+        System.out.println(DateUtil.getSdfTimes());
+        try {
+            PageData pd = new PageData();
+            //多数据源连接，erp数据库
+            List<PageData> jsonlist = itemService.listClient(pd);  //null换成service查询数据
+            jsonarr = JSONArray.fromObject(jsonlist);
+            //查询本地数据
+            List<PageData> varOList = clientService.listAll(pd);  //本地数据
+            System.out.println(varOList.size());
+            //新增开关
+            int hint = 0; //1为开启，0为关闭
+            //删除开关
+            int dint = 0; //开1,关0
+            //int dstr = 0;
+            int count = 0;
+            int dcount = 0;
+            int ecount = 0;
+            PageData pd3 = new PageData();
+            System.out.println(DateUtil.getSdfTimes());
+            if(jsonarr.size() > 0 ){
+                for (int i = 0; i < jsonarr.size(); i++) {
+                    hint = 1;
+                    JSONObject job = jsonarr.getJSONObject(i);
+                    if (varOList.size() > 0){
+                        for (int j = 0; j < varOList.size(); j++) {
+                            //判断本地数据和erp数据是否已经存在FITEMID
+                            if(varOList.get(j).get("FITEMID").equals(Integer.parseInt(job.get("FItemID").toString()))){
+                                //存在即把开关关闭
+                                hint = 0;
+                                //判断本地数据和erp的FMODIFYTIME和本地FMODIFYTIME是否相等，如果不相等即进行修改
+                                if (!varOList.get(j).get("FMODIFYTIME").equals(job.get("FModifyTime").toString())) {
+                                    pd3.put("CLIENT_ID", varOList.get(j).get("CLIENT_ID"));
+                                    pd3.put("FMODIFYTIME", job.get("FModifyTime").toString());
+                                    pd3.put("FNAME", job.getString("FName"));
+                                    pd3.put("FNUMBER", job.getString("FNumber"));
+                                    pd3.put("FITEMID", Integer.parseInt(job.get("FItemID").toString()));
+                                    pd3.put("FDELETED", Integer.parseInt(job.get("FDeleted").toString()));
+                                    pd3.put("FPARENTID", Integer.parseInt(job.get("FParentID").toString()));
+                                    //执行修改（找到对应的service）
+                                    //clientService.edit(pd3);
+                                    ecount ++ ;
+                                }
+                            }
+                        }
+                    }
+                    //如果上面没有把开关关闭，即执行保存
+                    if(hint == 1) {
+                        pd.put("CLIENT_ID", this.get32UUID());
+                        pd.put("FMODIFYTIME", job.get("FModifyTime").toString());
+                        pd.put("FNAME", job.getString("FName"));
+                        pd.put("FNUMBER", job.getString("FNumber"));
+                        pd.put("FITEMID", Integer.parseInt(job.get("FItemID").toString()));
+                        pd.put("FDELETED", Integer.parseInt(job.get("FDeleted").toString()));
+                        pd.put("FPARENTID", Integer.parseInt(job.get("FParentID").toString()));
+                        //执行保存（找到对应的service）
+                        clientService.save(pd);
+                        count++;
+                    }
+                }
+            }
+            System.out.println(DateUtil.getSdfTimes());
+            //在这里做一次嵌套for循环，必须分开做，反向判断
+            PageData pd2 = new PageData();
+            if(varOList.size() > 0){
+                for (int j = 0; j < varOList.size(); j++) {
+                    dint = 1;  //初始化开关
+                    if(jsonarr.size() > 0){
+                        for (int i = 0; i < jsonarr.size(); i++) {
+                            JSONObject job = jsonarr.getJSONObject(i);
+                            if(Integer.parseInt(job.get("FItemID").toString()) == Integer.parseInt(varOList.get(j).get("FITEMID").toString())){
+                                dint = 0; //erp存在改数据，关闭删除
+                            }
+                        }
+                        //当没有检查到erp存在对应FITEMID的话，关闭删除没有设定，执行删除
+                        if(dint == 1){
+                            pd2.put("FITEMID",Integer.parseInt(varOList.get(j).get("FITEMID").toString()));
+                            clientService.deleteByFITEMID(pd2);
+                            dcount ++ ;
+                        }
+                    }else {
+                        //逻辑全部删除
+                    }
+                }
+            }
+            System.out.println("=====数据处理完成=====");
+            System.out.println("新增数据"+count);
+            System.out.println("修改数据"+ecount);
+            System.out.println("删除数据"+dcount);
+            json.put("Data", "新增数据"+count+"条；"+"修改数据"+ecount+"条；"+"删除数据"+dcount+"条。");
+            System.out.println(DateUtil.getSdfTimes());
+            System.gc();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+    @RequestMapping(value="/test1")
+    @ResponseBody
+    public Map<String, Object> test1() throws Exception{
+        Map<String, Object> json = new HashMap<String, Object>();
+        String requestUrl = this.getIpAndProjectName()+"/erp_get/erp_cus";
+        //调用方式  0为远程接口方式，1为多数据源调用方式
+        String todoType = Tools.readTxtFile("admin/config/TYPE.txt");
+        JSONArray jsonarr = null;
+        System.out.println(DateUtil.getSdfTimes());
+        try {
+            PageData pd = new PageData();
+            //多数据源连接，erp数据库
+            List<PageData> jsonlist = itemService.listClient(pd);  //null换成service查询数据
+            jsonarr = JSONArray.fromObject(jsonlist);
+            //查询本地数据
+            List<PageData> varOList = clientService.listAll(pd);  //本地数据
+            //新增开关
+            int hint = 0; //1为开启，0为关闭
+            //删除开关
+            int dint = 0; //开1,关0
+            //int dstr = 0;
+            int count = 0;
+            int dcount = 0;
+            int ecount = 0;
+            StringBuffer strbuf = new StringBuffer();
+            JSONObject job = null;
+            PageData pd3 = new PageData();
+            List<PageData> strlist = new ArrayList<PageData>();
+            if(jsonarr.size() > 0 ){
+                int num = 100; //设置每次100条
+                int remainder = jsonarr.size() % num;  //取到余数
+                int numcount = jsonarr.size() / num;  //次数
+                for (int x = 0; x < numcount; x++) {
+                    strlist.clear();
+                    for (int i = num * x; i < num * (x + 1); i++) {
+                        hint = 1;
+                        job = jsonarr.getJSONObject(i);
+                        if (varOList.size() > 0){
+                            for (int j = 0; j < varOList.size(); j++) {
+                                //判断本地数据和erp数据是否已经存在FITEMID
+                                if(varOList.get(j).get("FITEMID").equals(Integer.parseInt(job.get("FItemID").toString()))){
+                                    //存在即把开关关闭
+                                    hint = 0;
+                                    //判断本地数据和erp的FMODIFYTIME和本地FMODIFYTIME是否相等，如果不相等即进行修改
+                                    if (!varOList.get(j).get("FMODIFYTIME").equals(job.get("FModifyTime").toString())) {
+                                        pd3.put("CLIENT_ID", varOList.get(j).get("CLIENT_ID"));
+                                        pd3.put("FMODIFYTIME", job.get("FModifyTime").toString());
+                                        pd3.put("FNAME", job.getString("FName"));
+                                        pd3.put("FNUMBER", job.getString("FNumber"));
+                                        pd3.put("FITEMID", Integer.parseInt(job.get("FItemID").toString()));
+                                        pd3.put("FDELETED", Integer.parseInt(job.get("FDeleted").toString()));
+                                        pd3.put("FPARENTID", Integer.parseInt(job.get("FParentID").toString()));
+                                        //执行修改（找到对应的service）
+                                        //clientService.edit(pd3);
+                                        ecount ++ ;
+                                    }
+                                }
+                            }
+                        }
+                        //如果上面没有把开关关闭，即执行保存
+                        if(hint == 1) {
+                            PageData pdCus = new PageData();
+                            pdCus.put("CLIENT_ID", this.get32UUID());
+                            pdCus.put("FMODIFYTIME", job.get("FModifyTime").toString());
+                            pdCus.put("FNAME", job.getString("FName"));
+                            pdCus.put("FNUMBER", job.getString("FNumber"));
+                            pdCus.put("FITEMID", Integer.parseInt(job.get("FItemID").toString()));
+                            pdCus.put("FDELETED", Integer.parseInt(job.get("FDeleted").toString()));
+                            pdCus.put("FPARENTID", Integer.parseInt(job.get("FParentID").toString()));
+                            strlist.add(pdCus);
+                            count++;
+                        }
+                    }
+                    //System.out.println(strlist);
+                    if(strlist.size() > 0){
+                        clientService.saveByList(strlist);
+                    }
+                }
+                strlist.clear();
+                for (int i = (jsonarr.size() - remainder); i < jsonarr.size(); i++) {
+                    hint = 1;
+                    job = jsonarr.getJSONObject(i);
+                    if (varOList.size() > 0){
+                        for (int j = 0; j < varOList.size(); j++) {
+                            //判断本地数据和erp数据是否已经存在FITEMID
+                            if(varOList.get(j).get("FITEMID").equals(Integer.parseInt(job.get("FItemID").toString()))){
+                                //存在即把开关关闭
+                                hint = 0;
+                                //判断本地数据和erp的FMODIFYTIME和本地FMODIFYTIME是否相等，如果不相等即进行修改
+                                if (!varOList.get(j).get("FMODIFYTIME").equals(job.get("FModifyTime").toString())) {
+                                    pd3.put("CLIENT_ID", varOList.get(j).get("CLIENT_ID"));
+                                    pd3.put("FMODIFYTIME", job.get("FModifyTime").toString());
+                                    pd3.put("FNAME", job.getString("FName"));
+                                    pd3.put("FNUMBER", job.getString("FNumber"));
+                                    pd3.put("FITEMID", Integer.parseInt(job.get("FItemID").toString()));
+                                    pd3.put("FDELETED", Integer.parseInt(job.get("FDeleted").toString()));
+                                    pd3.put("FPARENTID", Integer.parseInt(job.get("FParentID").toString()));
+                                    //执行修改（找到对应的service）
+                                    //clientService.edit(pd3);
+                                    ecount ++ ;
+                                }
+                            }
+                        }
+                    }
+                    //如果上面没有把开关关闭，即执行保存
+                    if(hint == 1) {
+                        PageData pdCus = new PageData();
+                        pdCus.put("CLIENT_ID", this.get32UUID());
+                        pdCus.put("FMODIFYTIME", job.get("FModifyTime").toString());
+                        pdCus.put("FNAME", job.getString("FName"));
+                        pdCus.put("FNUMBER", job.getString("FNumber"));
+                        pdCus.put("FITEMID", Integer.parseInt(job.get("FItemID").toString()));
+                        pdCus.put("FDELETED", Integer.parseInt(job.get("FDeleted").toString()));
+                        pdCus.put("FPARENTID", Integer.parseInt(job.get("FParentID").toString()));
+                        strlist.add(pdCus);
+                        count++;
+                    }
+                }
+                //System.out.println(strlist);
+                if(strlist.size() > 0){
+                    clientService.saveByList(strlist);
+                }
+            }
+            //在这里做一次嵌套for循环，必须分开做，反向判断
+            PageData pd2 = new PageData();
+            if(varOList.size() > 0){
+                for (int j = 0; j < varOList.size(); j++) {
+                    dint = 1;  //初始化开关
+                    if(jsonarr.size() > 0){
+                        for (int i = 0; i < jsonarr.size(); i++) {
+                            job = jsonarr.getJSONObject(i);
+                            if(Integer.parseInt(job.get("FItemID").toString()) == Integer.parseInt(varOList.get(j).get("FITEMID").toString())){
+                                dint = 0; //erp存在改数据，关闭删除
+                            }
+                        }
+                        //当没有检查到erp存在对应FITEMID的话，关闭删除没有设定，执行删除
+                        if(dint == 1){
+                            pd2.put("FITEMID",Integer.parseInt(varOList.get(j).get("FITEMID").toString()));
+                            clientService.deleteByFITEMID(pd2);
+                            dcount ++ ;
+                        }
+                    }else {
+                        //逻辑全部删除
+                    }
+                }
+            }
+            System.out.println("=====数据处理完成=====");
+            System.out.println("新增数据"+count);
+            System.out.println("修改数据"+ecount);
+            System.out.println("删除数据"+dcount);
+            json.put("Data", "新增数据"+count+"条；"+"修改数据"+ecount+"条；"+"删除数据"+dcount+"条。");
+            System.out.println(DateUtil.getSdfTimes());
+            System.gc();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
 }
